0999feb7a3db34438b872232eeb0b7d7e73264a9
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-02-05 12:47:30 -0500

    Added greetings for Alice and Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
-
+        Scanner myScanner = new Scanner(System.in);
+        String yourName;
+        
+        System.out.println("Please enter your name");
+        yourName = myScanner.nextLine();
+        
+        if (yourName.equals("Alice")) {
+            System.out.println("Hello there Alice");
+        } else if (yourName.equals("Bob")) {
+            System.out.println("Yo Bob");
+        } else {
+            System.out.println("You don't get a greeting");
+        }
     }
 }
